2dbc180178cb92b48d0fed4b900ddb88e01497f2
Aleksandr <mirrralex1@gmail.com>
2018-09-06 17:41:21 +0200

    Дописал попапы

--- js/popup.js
+++ js/popup.js
@@ -5,6 +5,8 @@ let popupCallWindow = document.getElementsByClassName('popup')[0],
 	contactUs = document.getElementsByClassName('contact_us')[0],
 	feedback = document.getElementsByClassName('feedback_block')[0],
 	statusMessage = document.createElement('div'),
+	popupConfWindow = document.getElementsByClassName('popup_engineer')[0],
+	callMaster = document.getElementsByClassName('header_btn_wrap_block')[0],
 	message = new Object();
 
 statusMessage.classList.add('status');
@@ -15,68 +17,100 @@ message.failure = 'К сожелению что-то пошло не так';
 
 function sendForm(element) {
 	let input = element.getElementsByTagName('input'),
-		inputName = input[0].value,
-		inputPhone = input[1].value;
-		message.txt = encodeURIComponent("Вам пришло сообщение от " + inputName + " что бы ему позвонить наберите " + inputPhone);
+		inputName = input[0],
+		inputPhone = input[1],
+		elementBtn = element.getElementsByClassName('btn-block')[0],
+		correctForm = element.getElementsByClassName('popup_form')[0];
+
+	elementBtn.disabled = true;
+
+	message.txt = encodeURIComponent("Вам пришло сообщение от " + inputName.value + " что бы ему позвонить наберите " + inputPhone.value);
 
 	element.style.display = 'block';
 
-	element.addEventListener('submit', function(elem) {
+	inputPhone.addEventListener('change', function() {
 
-		elem.preventDefault();
+	if (isNaN(inputPhone.value) || inputPhone == '') {
 
-		function postData(data) {
+		correctForm.style.backgroundColor = 'red';
 
-			return new Promise(function(resolve, reject) {
-				let request = new XMLHttpRequest();
+		elementBtn.disabled = true;
 
-				request.open('POST', 'server.php');
+		statusMessage.innerHTML = "Нужно ввести цифры а не буквы в графу введите телефон";
+	
+	} else {
 
-				request.setRequestHeader('Content-Type', 'aplication/x-www-form-urlencoded');
+		correctForm.style.backgroundColor = 'green';
 
-				request.onreadystatechange = function() {
+		elementBtn.disabled = false;
 
-					if (request.readyState < 4) {
-						resolve();
-					} else if (request.readyState === 4) {
-						if (request.status === 200 && request.status < 300) {
-							resolve();
-						} else {
-							reject();
-						}
+		statusMessage.innerHTML = "Все правильно, спасибо. Проверьте ваши данные и если они правильны смело нажимайте 'Заказать звонок'";
+
+		element.addEventListener('submit', function(elem) {
+
+				elem.preventDefault();
+
+				function postData(data) {
+
+					return new Promise(function(resolve, reject) {
+						let request = new XMLHttpRequest();
+
+						request.open('POST', 'server.php');
+
+						request.setRequestHeader('Content-Type', 'aplication/x-www-form-urlencoded');
+
+						request.onreadystatechange = function() {
+
+							if (request.readyState < 4) {
+								resolve();
+							} else if (request.readyState === 4) {
+								if (request.status === 200 && request.status < 300) {
+									resolve();
+								} else {
+									reject();
+								}
+							}
+						};
+
+						request.send(data);
+
+					});
+				} //End postData
+				function clearInput(){
+					for (let i = 0; i < input.length; i++) {
+					input[i].value = '';
 					}
-				};
+				}
 
-				request.send(data);
-
+				postData(message.txt)
+					.catch(() => statusMessage.innerHTML = "Нужно ввести цифры а не буквы в графу введите телефон")
+					.then( () => statusMessage.innerHTML = message.loading)
+					.then( () => {
+						statusMessage.innerHTML = message.success;
+						setTimeout( () => {
+							correctForm.style.backgroundColor = "white";
+							statusMessage.innerHTML = '';
+						}, 3000);
+						})
+					.catch( () => statusMessage.innerHTML = message.failure)
+					.then(clearInput);
 			});
-		} //End postData
-		function clearInput(){
-			for (let i = 0; i < input.length; i++) {
-			input[i].value = '';
-			}
 		}
-
-
-		postData(message.txt)
-			.then( () => statusMessage.innerHTML = message.loading)
-			.then( () => {
-				statusMessage.innerHTML = message.success;
-				setTimeout( () => {
-					statusMessage.innerHTML = '';
-				}, 3000);
-				})
-			.catch( () => statusMessage.innerHTML = message.failure)
-			.then(clearInput);
 	});
-}
 
+}
+//Заказать обратный звонок
 contactUs.addEventListener('click', function(){
 
 	sendForm(popupCallWindow);
 });
-
+//Спростие нашего специалиста
 feedback.addEventListener('click', function(){
+
+	sendForm(popupCallWindow);
+});
+//Заказать сегодня на замер
+callMaster.addEventListener('click', function(){
 
 	sendForm(popupCallWindow);
 });
@@ -105,4 +139,3 @@ function isDescendant(parent, child) {
      }
      return false;
 }
-//test
