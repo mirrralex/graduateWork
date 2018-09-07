dd8f3cc862ddcbe580b9ce88f3204eb1645f5bb3
Aleksandr <mirrralex1@gmail.com>
2018-09-07 12:16:57 +0200

    Добавил для 6 форм отправку

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,14 @@
 //Открытие popup а также отвравка формы
 let popupCallWindow = document.getElementsByClassName('popup')[0],
-	popupForm = popupCallWindow.getElementsByClassName('form')[0],
 	popupDialog = popupCallWindow.getElementsByClassName('popup_dialog')[0],
 	contactUs = document.getElementsByClassName('contact_us')[0],
 	feedback = document.getElementsByClassName('feedback_block')[0],
 	callMaster = document.getElementsByClassName('header_btn')[0],
-	statusMessage = document.createElement('div'),
-
+	mainForm = document.getElementsByClassName('col-lg-4 col-sm-8 col-sm-offset-2'),
 	message = new Object();
 
-statusMessage.classList.add('status');
-popupForm.appendChild(statusMessage);
+popupCallWindow.style.top = 'auto';
+popupCallWindow.style.left = 'auto';
 message.loading = 'Идет отправка';
 message.success = 'Спасибо, письмо отправлено';
 message.failure = 'К сожелению что-то пошло не так';
@@ -19,9 +17,15 @@ function sendForm(element) {
 	let input = element.getElementsByTagName('input'),
 		inputName = input[0],
 		inputPhone = input[1],
-		elementBtn = element.getElementsByTagName('button')[0];
+		popupForm = element.getElementsByClassName('form')[0],
+		statusMessage = document.createElement('div'),
+		elementBtn = element.getElementsByClassName('btn-block')[0];
 
-	elementBtn.style.disabled = true;
+	elementBtn.disabled = true;
+
+	statusMessage.classList.add('status');
+
+	popupForm.appendChild(statusMessage);
 
 	element.style.display = 'block';
 
@@ -31,13 +35,13 @@ function sendForm(element) {
 
 			statusMessage.innerHTML = "Введите пожалуйста ваш номер телефона, а не набор букв";
 
-			elementBtn.style.disabled = true;
+			elementBtn.disabled = true;
 
 			} else {
 
 				statusMessage.innerHTML = "Спасибо, теперь все правильно, проверьте ваши данные и если все правильно то смело нажимайте кнопку заказать звонок";
 
-				elementBtn.style.disabled = false;
+				elementBtn.disabled = false;
 
 				message.txt = encodeURIComponent("Вам пришло сообщение от " + inputName.value + " что бы ему позвонить наберите " + inputPhone.value);
 
@@ -122,7 +126,7 @@ popupCallWindow.addEventListener('click', function(elem) {
 	popupCallWindow.style.left = 'auto';
 	}
 });
-//Кнопка вызватьмастера такая зеленая сверху
+//Кнопка вызвать мастера такая зеленая сверху
 callMaster.addEventListener('click', function(){
 
 	sendForm(popupCallWindow);
@@ -134,14 +138,12 @@ setTimeout(function() {
 	sendForm(popupCallWindow);
 }, 60000);
 
+//Задание отправки формы для всех бланков
+for (let i = 0; i < mainForm.length; i++) {
 
+	sendForm(mainForm[i]);
 
-
-
-
-
-
-
+}
 
 //проверка на родителя
 function isDescendant(parent, child) {
