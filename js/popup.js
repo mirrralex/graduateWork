163a98c4ecbe850106de1e4830dd0476388ad35c
Aleksandr <mirrralex1@gmail.com>
2018-09-07 11:18:08 +0200

    Вернул код к задуманому екземпляру

--- js/popup.js
+++ js/popup.js
@@ -4,27 +4,51 @@ let popupCallWindow = document.getElementsByClassName('popup')[0],
 	popupDialog = popupCallWindow.getElementsByClassName('popup_dialog')[0],
 	contactUs = document.getElementsByClassName('contact_us')[0],
 	feedback = document.getElementsByClassName('feedback_block')[0],
+	callMaster = document.getElementsByClassName('header_btn')[0],
 	statusMessage = document.createElement('div'),
+
 	message = new Object();
 
 statusMessage.classList.add('status');
 popupForm.appendChild(statusMessage);
-message.loading = 'Загрузка...';
-message.success = 'Спасибо мы вам перезвоним';
+message.loading = 'Идет отправка';
+message.success = 'Спасибо, письмо отправлено';
 message.failure = 'К сожелению что-то пошло не так';
 
 function sendForm(element) {
 	let input = element.getElementsByTagName('input'),
-		inputName = input[0].value,
-		inputPhone = input[1].value;
-		message.txt = encodeURIComponent("Вам пришло сообщение от " + inputName + " что бы ему позвонить наберите " + inputPhone);
+		inputName = input[0],
+		inputPhone = input[1],
+		elementBtn = element.getElementsByTagName('button')[0];
+
+	elementBtn.style.disabled = true;
 
 	element.style.display = 'block';
 
+	inputPhone.addEventListener('change', function(){
+
+		if(isNaN(inputPhone.value) || inputPhone.value == '') {
+
+			statusMessage.innerHTML = "Введите пожалуйста ваш номер телефона, а не набор букв";
+
+			elementBtn.style.disabled = true;
+
+			} else {
+
+				statusMessage.innerHTML = "Спасибо, теперь все правильно, проверьте ваши данные и если все правильно то смело нажимайте кнопку заказать звонок";
+
+				elementBtn.style.disabled = false;
+
+				message.txt = encodeURIComponent("Вам пришло сообщение от " + inputName.value + " что бы ему позвонить наберите " + inputPhone.value);
+
+			}
+	});
+		
 	element.addEventListener('submit', function(elem) {
 
 		elem.preventDefault();
 
+		
 		function postData(data) {
 
 			return new Promise(function(resolve, reject) {
@@ -57,7 +81,6 @@ function sendForm(element) {
 			}
 		}
 
-
 		postData(message.txt)
 			.then( () => statusMessage.innerHTML = message.loading)
 			.then( () => {
@@ -68,7 +91,9 @@ function sendForm(element) {
 				})
 			.catch( () => statusMessage.innerHTML = message.failure)
 			.then(clearInput);
+	
 	});
+
 }
 
 contactUs.addEventListener('click', function(){
@@ -89,18 +114,35 @@ popupCloseBotton.addEventListener('click', function() {
 	popupCallWindow.style.display = 'none';
 });
 
-//60 секундный popup
-setTimeout(function() {
-
-	sendForm(popupCallWindow);
-}, 6000);
-
 //Подложка
 popupCallWindow.addEventListener('click', function(elem) {
 	if (!isDescendant(popupDialog, elem.target)){
 	popupCallWindow.style.display = 'none';
+	popupCallWindow.style.top = 'auto';
+	popupCallWindow.style.left = 'auto';
 	}
 });
+//Кнопка вызватьмастера такая зеленая сверху
+callMaster.addEventListener('click', function(){
+
+	sendForm(popupCallWindow);
+});
+
+//60 секундный popup
+setTimeout(function() {
+
+	sendForm(popupCallWindow);
+}, 60000);
+
+
+
+
+
+
+
+
+
+
 //проверка на родителя
 function isDescendant(parent, child) {
      var node = child.parentNode;
