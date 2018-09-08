1b90596ce82b1f48b2706b1925294ce2f0c08873
Aleksandr <mirrralex1@gmail.com>
2018-09-08 11:56:47 +0200

    Добавил табы для уголков окон

--- js/tab.js
+++ js/tab.js
@@ -3,7 +3,8 @@ function tab(){
 	let glassSlide = document.getElementsByClassName('glazing_block'),
 		glassContentWarm = document.getElementsByClassName('glazing_warm'),
 		glassContentCold = document.getElementsByClassName('glazing_cold'),
-		glassContentPrice = document.getElementsByClassName('glazing_price'),
+		glassContentPriceWarm = document.getElementsByClassName('glazing_price warm'),
+		glassContentPriceCold = document.getElementsByClassName('glazing_price cold'),
 		glassWindow = document.getElementsByClassName('glazing_slider')[0],
 		glassImage = document.getElementsByClassName('imgOfwork'),
 		glassType = document.getElementsByClassName('typeOfwork');
@@ -14,10 +15,32 @@ function tab(){
 
 		for(let i = elementNumber; i < glassContentWarm.length; i++) {
 
-			glassSlide[i].classList.add('active');
+			glassSlide[i].classList.remove('active');
 
-			console.log('yeap');
+			glassType[i].classList.remove('active');
 
+			glassContentWarm[i].classList.add('hide');
+			glassContentWarm[i].classList.remove('show');
+
+			glassContentPriceWarm[i].classList.add('hide');
+			glassContentPriceWarm[i].classList.remove('show');
+
+			if( i < 3) {
+
+				glassContentCold[i].classList.add('hide');
+				glassContentCold[i].classList.remove('show');
+
+				glassContentPriceCold[i].classList.add('hide');
+				glassContentPriceCold[i].classList.remove('show');
+
+			} else if (i > 3) {
+
+				glassContentCold[i-1].classList.add('hide');
+				glassContentCold[i-1].classList.remove('show');
+
+				glassContentPriceCold[i-1].classList.add('hide');
+				glassContentPriceCold[i-1].classList.remove('show');
+			}
 		}
 	}
 
@@ -31,15 +54,36 @@ function tab(){
 
 			glassSlide[number].classList.add('active');
 
-			console.log('ugu');
+			glassType[number].classList.add('active');
+
+			glassContentWarm[number].classList.remove('hide');
+			glassContentWarm[number].classList.add('show');
+
+			glassContentPriceWarm[number].classList.remove('hide');
+			glassContentPriceWarm[number].classList.add('show');
+
+			if( number < 2) {
+
+				glassContentCold[number].classList.remove('hide');
+				glassContentCold[number].classList.add('show');
+
+				glassContentPriceCold[number].classList.remove('hide');
+				glassContentPriceCold[number].classList.add('show');
+
+			} else if (number > 2) {
+
+				glassContentCold[number-1].classList.remove('hide');
+				glassContentCold[number-1].classList.add('show');
+
+				glassContentPriceCold[number-1].classList.remove('hide');
+				glassContentPriceCold[number-1].classList.add('show');
+			}
 		}
 	}
 
 	glassWindow.addEventListener('click', function(event) {
 		
 		let target = event.target;
-
-		console.log('aga');
 
 		if(target.classList.contains('glazing_block') || target.classList.contains('imgOfwork')|| target.classList.contains('typeOfwork')) {
 
@@ -48,8 +92,6 @@ function tab(){
 				if (target == glassType[i] || target == glassImage[i]) {
 
 					tabOpen(i);
-
-					console.log('grea');
 
 					break;
 
